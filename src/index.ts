1db228934f814c42b80898ce900dbd67f923c58e
Izuho Fujiwara <fujishan2212@gmail.com>
2022-04-03 02:20:44 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import { getCurrentBlocknumber } from "./functions/get-current-blocknumber"
-import { storeCurrentBlocknumber } from "./functions/store-current-blocknumber";
-
 export const exec = async () => {
-	console.log(`CurrentBlockNumber: ${await getCurrentBlocknumber('polygon')}`);
-
-	await storeCurrentBlocknumber('polygon', 500);
-
-	console.log(`CurrentBlockNumber: ${await getCurrentBlocknumber('polygon')}`);
+	//
 }
