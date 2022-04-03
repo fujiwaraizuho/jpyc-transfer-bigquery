d572d0ca527e0e95e624e9ae8ab40c3b88a415bc
Izuho Fujiwara <fujishan2212@gmail.com>
2022-04-04 02:56:51 +0900

    add archive script

---
+++ scripts/archive.ts
@@ -0,0 +1,95 @@
+import { AlchemyProvider } from "@ethersproject/providers";
+import { Contract } from "ethers";
+import { getPastTransferEvent } from "../src/functions/get-past-transfer-events";
+import { chunkArray } from "../src/utils";
+import { insertTransferEvents } from "../src/functions/insert-transfer-events";
+
+import jpycTokenAbi from '../src/abis/jpycToken.json';
+
+/* CONFIG */
+const network = 'polygon'
+
+const alchemyNetworkName = 'matic';
+const alchemyApiKey = '';
+
+const jpycContractAddress = '0x6AE7Dfc73E0dDE2aa99ac063DcF7e8A63265108c';
+
+/* ENTRY FUNCTION */
+export const selfExec = async (previousBlockNumber: number, currentBlockNumber: number) => {
+	console.info(`JPYC TransferEvent to BigQuery Batch [${network}]`);
+	console.info('--- STEP1. Prepare BlockNumber ---');
+
+	const provider = new AlchemyProvider(alchemyNetworkName, alchemyApiKey);
+
+	// 前回処理した最終ブロック高を取得
+	console.info(`-> Previous BlockNumber: ${previousBlockNumber}`);
+
+	// 現在のブロック高を取得
+	console.info(`-> Current BlockNumber: ${currentBlockNumber}`);
+
+	// 今回の処理でターゲットにするブロック高を計算
+	const targetBlockNumber = currentBlockNumber;
+	console.info(`-> Target BlockNumber: ${targetBlockNumber}`);
+
+	// 現在のブロック高が前回のブロック高と同じかそれより小さいときは処理しない
+	if (previousBlockNumber >= targetBlockNumber) {
+		console.info('-> Not enough difference.');
+		console.info('-------------------------');
+		return;
+	}
+
+	console.info(`-> Difference Block Number: ${targetBlockNumber - previousBlockNumber}`);
+
+	console.info('--- STEP2. Get Transfer Event ---');
+
+	const jpycTokenContract = new Contract(jpycContractAddress, jpycTokenAbi, provider);
+
+	// 最終処理ブロック高と現在のブロック高の間の TransferEvent を取得
+	const txs = await getPastTransferEvent(provider, jpycTokenContract, previousBlockNumber + 1, targetBlockNumber);
+
+	console.info(`-> TransferEvent TXS Length: ${txs.length}`);
+
+	// 該当トランザクションが存在しなかったら処理しない
+	if (txs.length <= 0) {
+		console.info('-> Not enough transactions.');
+		console.info('-------------------------');
+		return;
+	}
+
+	// 取得したイベントが 100 件以上の配列になった場合は分割する
+	const chunkedTxs = chunkArray(txs, 100);
+
+	console.info(`-> Chunked TXS Length: ${chunkedTxs.length}`);
+
+	console.info('--- STEP3. Store BigQuery ---');
+
+	let counter = 0;
+
+	for (const chunkdTx of chunkedTxs) {
+		counter += chunkdTx.length;
+		console.info(`-> Processing... [${counter}/${txs.length}]`);
+
+		await insertTransferEvents(network, chunkdTx);
+
+		console.info(`-> Processed Transaction [${counter}/${txs.length}]`);
+	}
+
+	console.info('-------------------------');
+}
+
+const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+(async () => {
+	// 10000000 -> 2670000 を 20000block ごと取得
+	const max = 26700000;
+
+	let tmpCount = 0;
+
+	for (let i = 10000000; i <= max; i += 20000) {
+		await selfExec(tmpCount + 1, i);
+
+		tmpCount = i;
+
+		await _sleep(10000);
+	}
+})();
--- src/functions/get-previous-blocknumber.ts
+++ src/functions/get-previous-blocknumber.ts
@@ -1,6 +1,6 @@
 import { Datastore } from "@google-cloud/datastore"
 
-export const getPreviousBlocknumber = async (network: string): Promise<number> => {
+export const getPreviousBlocknumber = async (network: string): Promise<string> => {
 	const datastore = new Datastore();
 
 	const query = datastore.createQuery('PreviousBlockNumber')
@@ -12,5 +12,5 @@ export const getPreviousBlocknumber = async (network: string): Promise<number> =
 
 	const result = await datastore.runQuery(query);
 
-	return result[0][0].blocknumber ?? 0;
+	return result[0][0].blocknumber;
 }
